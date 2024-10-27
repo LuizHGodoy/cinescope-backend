98038ee5698665ba7b2f99164ba53e2a8560e782
Luiz_Godoy <luizhenriquegodoydeoliveira03@gmail.com>
2024-10-27 18:33:31 -0400

    remove unused worker && add swagger docs

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,6 +1,8 @@
 import { Controller, Delete, Get, Logger, Post, Query } from "@nestjs/common";
+import { ApiOperation, ApiResponse, ApiTags } from "@nestjs/swagger";
 import { MoviesService } from "./movies.service";
 
+@ApiTags("movies")
 @Controller("movies")
 export class MoviesController {
   private readonly logger = new Logger(MoviesController.name);
@@ -8,17 +10,32 @@ export class MoviesController {
   constructor(private readonly moviesService: MoviesService) {}
 
   @Post("reindex")
+  @ApiOperation({ summary: "Reindexa os filmes" })
+  @ApiResponse({
+    status: 200,
+    description: "Reindexação concluída com sucesso.",
+  })
   async reindex() {
     return this.moviesService.reindexMovies();
   }
 
   @Delete("clear")
+  @ApiOperation({ summary: "Limpa o banco de dados de filmes" })
+  @ApiResponse({
+    status: 200,
+    description: "Banco de dados de filmes limpo com sucesso.",
+  })
   async clearDatabase() {
     await this.moviesService.clearMoviesDatabase();
     return { message: "Banco de dados de filmes limpo com sucesso" };
   }
 
   @Get()
+  @ApiOperation({ summary: "Obtém a lista de filmes" })
+  @ApiResponse({
+    status: 200,
+    description: "Lista de filmes retornada com sucesso.",
+  })
   async getMovies(@Query("page") page = "1", @Query("limit") limit = "10") {
     const pageNumber = Number.parseInt(page, 10);
     const limitNumber = Number.parseInt(limit, 10);
